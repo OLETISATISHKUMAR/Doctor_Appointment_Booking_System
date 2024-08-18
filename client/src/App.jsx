10929d0addac1837cde12014b7200e37a3f5b0ad
OLETISATISHKUMAR <satisholeti1020@gmail.com>
2024-08-18 15:22:03 +0530

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Doctor Appointment Booking System
+      </h1>
+    </div>
+  );
+}
+
+export default App;
